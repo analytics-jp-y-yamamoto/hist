ffb0a94bb1bf1d6066ddb952fb598475519c884f
analytics-jp-y-yamamoto <129849323+analytics-jp-y-yamamoto@users.noreply.github.com>
2023-04-13 17:23:37 +0900

    Add files via upload

--- hist.py
+++ hist.py
@@ -6,11 +6,11 @@ import japanize_matplotlib
 
 # ページのタイトル設定
 st.set_page_config(
-    page_title="histogram",
+    page_title = "histogram",
 )
 
 # csv読み込み
-df0 = pd.read_csv('hist.csv', index_col=0)
+df0 = pd.read_csv('hist1.csv', index_col=0)
 
 # セッション情報の初期化
 if "page_id" not in st.session_state:
@@ -30,26 +30,26 @@ st.markdown(hide_style, unsafe_allow_html=True)
 def main_page():
     st.markdown(
         "<h1 style='text-align: center;'>ヒストグラム表示</h1>",
-        unsafe_allow_html=True,
+        unsafe_allow_html = True,
     )
 
-    column_list=st.session_state.df0.columns.values
-    Day_list=st.session_state.df0.index.values
-    Day_list_selector=st.sidebar.selectbox( "データセット選択",Day_list)
+    column_list = st.session_state.df0.columns.values
+    data_list = st.session_state.df0.index.values
+    data_list_selector = st.sidebar.selectbox( "データセット選択", data_list)
     bin = st.sidebar.slider('binの幅を決めてください', 5, 100, 25)
 
-    hist_array=[]
-    for i in range(2000):
-        hist_array.append(st.session_state.df0[column_list[i]][Day_list_selector])
+    hist_array = []
+    for i in range(st.session_state.df0.shape[1]):
+        hist_array.append(st.session_state.df0[column_list[i]][data_list_selector])
 
-    Y, X, _ = plt.hist(hist_array,bins=bin, range=(-1, 1))
+    Y, X, _ = plt.hist(hist_array, bins = bin, range = (-1, 1))
     y_max = int(max(Y)) + 1
 
     fig1, ax1 = plt.subplots()
 
-    ax1.hist(hist_array,bins=bin,ec='navy', range=(-1, 1))
-    ax1.set_title("標準正規分布2000個データ")
-    ax1.set_xlabel(Day_list_selector)
+    ax1.hist(hist_array, bins = bin, ec = 'navy', range = (-1, 1))
+    ax1.set_title("標準正規分布"+str(st.session_state.df0.shape[1])+"個データ")
+    ax1.set_xlabel(data_list_selector)
     ax1.set_ylabel("度数")
     ax1.set_yticks(np.arange(0,y_max,int(y_max/10)))
 
@@ -58,3 +58,7 @@ def main_page():
 # ページ判定
 if st.session_state.page_id == -1:
     main_page()
+
+    #タイタニックデータセットの分析
+    #コードの修正
+    #なんでこういうデザインにしたのか？
